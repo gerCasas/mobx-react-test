e44054f61935d849d329158d4659c48950c47ff4
gerCasas <gcasasg4@gmail.com>
2017-02-10 14:43:29 -0800

    1st commit
    Hola mundo en Todo List

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+import "../css/main.css"
+import React from "react"
+import ReactDOM from "react-dom"
+import TodoList from "./TodoList"
+
+const app = document.getElementById("app")
+
+ReactDOM.render(<TodoList />, app)
